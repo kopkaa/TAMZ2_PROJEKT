3220461a6a5b87564bfc3135eea3d39ab06c8cf7
Jan Hromádka <cumlikator1@email.cz>
2020-10-27 20:49:37 +0100

    menu Background

--- android/src/com/hroo078/gxattack/Game/GallaxyAttackGame.java
+++ android/src/com/hroo078/gxattack/Game/GallaxyAttackGame.java
@@ -1,5 +1,7 @@
 package com.hroo078.gxattack.Game;
 
+import android.content.res.Resources;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -9,6 +11,7 @@ import com.hroo078.gxattack.Game.Screens.MenuScreen;
 public class GallaxyAttackGame extends Game {
 
 	static public Skin gameSkin;
+
 	@Override
 	public void create() {
 		gameSkin = new Skin(Gdx.files.internal("skin/uiskin.json"));
--- android/src/com/hroo078/gxattack/Game/Screens/MenuScreen.java
+++ android/src/com/hroo078/gxattack/Game/Screens/MenuScreen.java
@@ -1,21 +1,18 @@
 package com.hroo078.gxattack.Game.Screens;
 
-import android.view.InputEvent;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.hroo078.gxattack.Game.GallaxyAttackGame;
 
-import org.w3c.dom.Text;
+
 
 import java.util.ArrayList;
 
@@ -23,33 +20,55 @@ public class MenuScreen implements Screen {
 
     private Game game;
     private Stage stage;
-    private ArrayList<TextButton> menuButtons = new ArrayList<TextButton>();
+
+    // Background
+    private Texture background;
+    private SpriteBatch batch;
+    private int backgroundOffset;
+
+    // Buttons
+    private ArrayList<TextButton> menuButtons = new ArrayList<>();
+    private  float BUTTON_WIDTH = 300f;
+    private  float BUTTON_HEIGHT = 65;
 
     public MenuScreen(Game game) {
         this.game = game;
         stage = new Stage();
+        initBackground();
         initButtons();
         Gdx.input.setInputProcessor(stage);
     }
 
+    public void initBackground() {
+        background = new Texture("Space_Stars2.png");
+        backgroundOffset = 0;
+        batch = new SpriteBatch();
+    }
+
     private void initButtons() {
 
-        menuButtons.add(new TextButton("Play game",GallaxyAttackGame.gameSkin, "default"));
-        menuButtons.add(new TextButton("High Score",GallaxyAttackGame.gameSkin, "default"));
-        menuButtons.add(new TextButton("Options",GallaxyAttackGame.gameSkin, "default"));
-        menuButtons.add(new TextButton("About",GallaxyAttackGame.gameSkin, "default"));
+        VerticalGroup group = new VerticalGroup();
+        int groupHeight = 0;
 
+        menuButtons.add(new TextButton("Play game", GallaxyAttackGame.gameSkin, "default"));
+        menuButtons.add(new TextButton("High Score", GallaxyAttackGame.gameSkin, "default"));
+        menuButtons.add(new TextButton("Options", GallaxyAttackGame.gameSkin, "default"));
+        menuButtons.add(new TextButton("About", GallaxyAttackGame.gameSkin, "default"));
 
+        for(int i = 0; i <= menuButtons.size()-1; i++) {
+            Container<TextButton> container = new Container<TextButton>(menuButtons.get(i));
+            container.width(BUTTON_WIDTH);
+            container.height(BUTTON_HEIGHT);
+            container.padBottom(20);
 
+            groupHeight += BUTTON_HEIGHT + 20;
+            group.addActor(container);
+        }
 
+        System.out.println("GROUP HEIGHT" + groupHeight);
+        group.setPosition(Gdx.graphics.getWidth() /2 - (group.getWidth() / 2),Gdx.graphics.getHeight()/2  + groupHeight / 2);
 
-//        for(int i=0; i<=3; i++) {
-//            TextButton btn = new TextButton("css",GallaxyAttackGame.gameSkin, "default");
-//            btn.setWidth(200f);
-//            btn.setHeight(20f);
-//            btn.setPosition(Gdx.graphics.getWidth() / 2 - 50*i, Gdx.graphics.getHeight() / 2 - 10*i);
-//            stage.addActor(btn);
-//        }
+        stage.addActor(group);
     }
 
     @Override
@@ -61,7 +80,15 @@ public class MenuScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
 
+        backgroundOffset++;
+        if(backgroundOffset >= Gdx.graphics.getHeight()) {
+            backgroundOffset = 0;
+        }
+        batch.draw(background,0,-backgroundOffset,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(background,0,-backgroundOffset+Gdx.graphics.getHeight(),Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
         stage.draw();
     }
 
